83d97734bfd55f2f08e499563957847c2a33f78a
StellarH <107297301+StellarH@users.noreply.github.com>
2022-11-27 15:36:08 +0800

    Add files via upload

--- src/indi/blazing/stellar/document/STEText.java
+++ src/indi/blazing/stellar/document/STEText.java
@@ -1,3 +1,20 @@
+/**
+ ******************************************************************************
+ * Copyright (c) 2022 BlazingStellar
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the 
+ * 		Eclipse Public License 2.0 
+ * which is available at 
+ * 		http://www.eclipse.org/legal/epl-2.0 
+ * or the 
+ * 		BSD 3-Clause License 
+ * which is available at 
+ * 		https://opensource.org/licenses/BSD-3-Clause 
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ ******************************************************************************
+ */
 package indi.blazing.stellar.document;
 
 import java.io.BufferedReader;
